96f237b5b98e6f1424af8e1f13626009f76bd666
Nita Catalin <nitzacatalin@yahoo.com>
2021-11-22 13:19:50 +0200

    Update orWhereFuzzy search logic Add test cases for updates Update test table, add nickname column Update orderByFuzzy

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,10 +1,10 @@
-<?php declare(strict_types = 1);
+<?php declare(strict_types=1);
 
 namespace Quest;
 
-use Quest\Macros\WhereFuzzy;
-use Quest\Macros\OrderByFuzzy;
 use Illuminate\Database\Query\Builder;
+use Quest\Macros\OrderByFuzzy;
+use Quest\Macros\WhereFuzzy;
 use Illuminate\Support\ServiceProvider as Provider;
 
 class ServiceProvider extends Provider
@@ -14,10 +14,26 @@ class ServiceProvider extends Provider
      * Bootstrap any application services.
      *
      **/
-    public function boot() : void
+    public function boot(): void
     {
         Builder::macro('orderByFuzzy', fn($fields) => OrderByFuzzy::make($this, $fields));
-        Builder::macro('whereFuzzy', fn($field, $value) => WhereFuzzy::make($this, $field, $value));
-        Builder::macro('orWhereFuzzy', fn($field, $value) => WhereFuzzy::makeOr($this, $field, $value));
+
+        Builder::macro('whereFuzzy', function ($field, $value = null) {
+            // check if first param is a closure and execute it if it is, passing the current builder as parameter
+            // so when $query->orWhereFuzzy, $query will be the current query builder, not a new instance
+            if ($field instanceof \Closure) {
+                $field($this);
+                return $this;
+            }
+            // if $query->orWhereFuzzy is called in the closure, or directly by the query builder, do this
+            return WhereFuzzy::make($this, $field, $value);
+        });
+        Builder::macro('orWhereFuzzy', function ($field, $value = null) {
+            if ($field instanceof \Closure) {
+                $field($this);
+                return $this;
+            }
+            return WhereFuzzy::makeOr($this, $field, $value);
+        });
     }
 }
--- support/migrations/2014_10_12_000000_create_users_table.php
+++ support/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function(Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('nickname');
             $table->string('country');
         });
     }
