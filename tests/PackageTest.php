e1253a90f8f4e12683387dffa58403ccdfb2d36a
pbringetto <50182684+pbringetto@users.noreply.github.com>
2023-09-13 14:50:01 -0700

    Update PackageTest.php

--- tests/PackageTest.php
+++ tests/PackageTest.php
@@ -24,7 +24,7 @@ class PackageTest extends TestCase
             'port'           => env('DB_PORT', 3306),
             'database'       => env('DB_DATABASE', 'testing'),
             'username'       => env('DB_USERNAME', 'root'),
-            'password'       => env('DB_PASSWORD', 'root'),
+            'password'       => env('DB_PASSWORD', ''),
             'unix_socket'    => env('DB_SOCKET', ''),
             'charset'        => 'utf8mb4',
             'collation'      => 'utf8mb4_unicode_520_ci',
